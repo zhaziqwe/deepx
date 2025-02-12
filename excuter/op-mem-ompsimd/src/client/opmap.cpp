e17e15f537b1681738163544916d86d3ebc435db
lipeng <734991033@qq.com>
2025-02-12 19:01:10 +0800

    deepx:删除冗余仓库

---
+++ excuter/op-mem-ompsimd/src/client/opmap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lipeng on 25-2-10.
+//
+
+#include "opmap.h"
---
+++ excuter/op-mem-ompsimd/src/client/opmap.h
@@ -0,0 +1,16 @@
+//
+// Created by lipeng on 25-2-10.
+//
+
+#ifndef OPMAP_H
+#define OPMAP_H
+
+
+
+class opmap {
+
+};
+
+
+
+#endif //OPMAP_H
