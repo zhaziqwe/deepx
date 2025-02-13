5d31751b19e665b1166a0fa870aae467a01c9c00
lipeng <734991033@qq.com>
2025-02-13 22:58:56 +0800

    deepx:readme

--- setup_commands.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p /home/lipeng/code/ai/deepx/front/py/deepx/graph
-touch /home/lipeng/code/ai/deepx/front/py/deepx/graph/__init__.py
-touch /home/lipeng/code/ai/deepx/front/py/deepx/graph/node.py
-touch /home/lipeng/code/ai/deepx/front/py/deepx/graph/graph.py
-touch /home/lipeng/code/ai/deepx/front/py/deepx/graph/ops.py 
