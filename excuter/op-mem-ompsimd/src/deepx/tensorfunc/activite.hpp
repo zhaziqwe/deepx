0f88ef227a17abf880ee313914b4cece2d28efa1
lipeng <734991033@qq.com>
2025-02-21 22:54:07 +0800

    op:transpose

--- excuter/op-mem-ompsimd/src/deepx/tensorfunc/activite.hpp
+++ excuter/op-mem-ompsimd/src/deepx/tensorfunc/activite.hpp
@@ -6,6 +6,11 @@
 
 namespace deepx::tensorfunc
 {   
+    /*
+    ReLU不是素Op,可以用Max_scalar组合实现:
+    ReLU(x) = Max(x, 0)
+    ReLU'(x) = x > 0 ? 1 : 0
+    */
     template<typename T>
     void reluInplace(Tensor<T> &tensor){
         tensor.shape.rangeParallel(tensor.shape.dim, [&tensor](int i){
@@ -35,21 +40,29 @@ namespace deepx::tensorfunc
     }
 
     /*
-    gelu 激活函数
-    平滑性：GELU是一个平滑的激活函数，能够在输入接近零时提供更好的梯度传播。
-    非线性：GELU在负输入时会输出负值，这使得它在处理负输入时比ReLU更灵活。
-    高斯特性：GELU结合了高斯分布的特性，能够在训练过程中更好地捕捉数据的分布。
+    GELU是素Op,因为其包含了erf函数,不能用基本算术运算组合:
+    GELU(x) = x * Φ(x)
+    其中Φ(x)是标准正态分布的累积分布函数
+    GELU'(x) = Φ(x) + x * φ(x)
+    其中φ(x)是标准正态分布的概率密度函数
     */
     void geluInplace(Tensor<float> &tensor);
     void geluGradInplace(Tensor<float> &tensor);
+
     /*
-    swiGlu  激活函数
-    swiGlu 对于负值的响应相对较小克服了 ReLU 某些神经元上输出始终为零的缺点
-    计算效率相比某些较复杂的激活函数（如 GELU）更高 
+    SwiGLU不是素Op,可以用乘法和Sigmoid组合实现:
+    SwiGLU(x) = x * sigmoid(β*x)
+    其中β是一个可调参数
+    SwiGLU'(x) = sigmoid(β*x) + β*x*sigmoid(β*x)*(1-sigmoid(β*x))
     */
     void swiGluInplace(Tensor<float> &tensor);
     void swiGluGradInplace(Tensor<float> &tensor);
 
+    /*
+    Sigmoid是素Op,因为其包含了指数函数,不能用基本算术运算组合:
+    Sigmoid(x) = 1/(1+e^(-x))
+    Sigmoid'(x) = sigmoid(x)*(1-sigmoid(x))
+    */
     void sigmoidInplace(Tensor<float> &tensor);
     void sigmoidGradInplace(Tensor<float> &tensor);
 }
