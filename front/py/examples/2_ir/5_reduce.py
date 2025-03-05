893bf92d7c439d40b17cbbb398c6470a43ae9699
lipeng <734991033@qq.com>
2025-03-05 13:17:14 +0800

    py:优化tensor计算他的node.name体系

---
+++ front/py/examples/2_ir/5_reduce.py
@@ -0,0 +1,11 @@
+from deepx import Tensor,ones
+from deepx.nn.functional import relu
+
+t=Tensor(shape=(10,10))
+t.uniform_(low=-1,high=1)
+print((t))
+relu_t=relu(t)
+print(relu_t)
+
+gviz=relu_t.graph.to_dot()
+gviz.render('relu.dot',format='svg')
