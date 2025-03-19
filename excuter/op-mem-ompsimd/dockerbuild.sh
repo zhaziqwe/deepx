b2aa9cf54f480b0bfade349fd2e0ed3f8a12d0be
miaobyte <734991033@qq.com>
2025-03-19 22:49:11 +0800

    build:docker build try fix

--- excuter/op-mem-ompsimd/dockerbuild.sh
+++ excuter/op-mem-ompsimd/dockerbuild.sh
@@ -1,2 +1,4 @@
-docker build -t docker.array2d.com/deepx/ompsimd:latest .
-docker push docker.array2d.com/deepx/ompsimd:latest
+cd ../
+pwd
+ls -al
+docker build -t docker.array2d.com/deepx/ompsimd:latest . -f op-mem-ompsimd/Dockerfile
